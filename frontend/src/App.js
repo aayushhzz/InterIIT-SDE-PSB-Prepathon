e3fdede757850be32c4cf6c12785c7f663b226ca
Aayush <66545158+aayushhzz@users.noreply.github.com>
2024-09-23 18:47:29 +0530

    test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,31 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import FileUploadPage from './pages/FileUploadPage';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/" element={<FileUploadPage />} />
-      </Routes>
+      <div className="App">
+        <div className="video-background">
+          <video
+            autoPlay
+            loop
+            muted
+            playsInline
+            className="background-video"
+            width={"100%"}
+          >
+            <source src="https://www.youtube.com/watch?v=SDxmkJwrbno&ab_channel=KarenHovsepyan" type="video/mp4" />
+          </video>
+        </div>
+        
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/" element={<FileUploadPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
