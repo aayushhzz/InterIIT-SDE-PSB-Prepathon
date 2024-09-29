3ee7f342973581f3f7d7d282001d808907e7be75
ApoorvDubey23 <apoorvd2301@gmail.com>
2024-09-29 11:14:12 +0530

    Added Auth0

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -3,10 +3,12 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import GlassCard from '../components/GlassCard';
 import '../App.css';
+import { useAuth0 } from "@auth0/auth0-react";
 
 const LoginPage = () => {
   const [password, setPassword] = useState('');
   const [username, setUsername] = useState('');
+  const { loginWithRedirect } = useAuth0();
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -17,10 +19,11 @@ const LoginPage = () => {
     <div className="auth-container">
       <GlassCard>
         <h2>Login</h2>
-        <form onSubmit={handleLogin}>
+        <form onSubmit={handleLogin} >
           <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
           <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
           <button type="submit">Login</button>
+          <button onClick={() => loginWithRedirect()}>Log In , in other ways</button>
         </form>
         <p>
           Don't have an account? <Link to="/register">Register</Link>
